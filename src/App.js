6692708f1b9b418833cd58044d685e75532679d7
rhdholariya <rahuldholariya001@gmail.com>
2023-05-29 10:58:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+const App = () => {
+  const [count, setCount] = React.useState(0);
+
+  const components = [
+    <div>
+      <h2><b>Q:1 What Is Your Hobby</b></h2>
+      <input type="radio"/>Music
+      <input type="radio"/>Cricket
+      <input type="radio"/>Dance
+    </div>,
+    <div>
+      <h2><b>Q:2 Where Is Your City</b></h2>
+        <input type="radio"/>Navsari
+        <input type="radio"/>Surat
+        <input type="radio"/>Baroda
+    </div>,
+
+    <div>
+      <h2><b>Q:3 What is your Name</b></h2>
+      <input type="radio"/>Mayuri
+      <input type="radio"/>Resu
+      <input type="radio"/>Pratik
+    </div>,
+  ]
+
+  return<div>
+    {
+      // render component from our components array
+      components[count]
+    }
+
+    {/* show previous button if we are not on first element */}
+    {count > 0 && <button onClick={() => setCount(count-1 )}>prev</button>}
+
+    {/* hide next button if we are at the last element */}
+    {count < components.length - 1 && <button onClick={() => setCount(count + 1)}>next</button>}
+  </div>
 }
-
-export default App;
+export  default App;
