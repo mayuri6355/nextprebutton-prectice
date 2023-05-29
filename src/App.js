ad85df1911bd2833027de2e5238e3f97111f1655
rhdholariya <rahuldholariya001@gmail.com>
2023-05-29 11:16:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
+import {useState} from "react";
 
-import React from "react";
 const App = () => {
-  const [count, setCount] = React.useState(0);
+  const [count,setCount]=useState(0)
+  const [gender, setGender] =useState("music");
+
+  function onChangeValue(event) {
+    setGender(event.target.value);
+
+  }
 
   const components = [
     <div>
       <h2><b>Q:1 What Is Your Hobby</b></h2>
-      <input type="radio"/>Music
-      <input type="radio"/>Cricket
-      <input type="radio"/>Dance
+      <input type="radio" value="music"  onChange={onChangeValue} checked={gender === "music"} />Music
+      <input type="radio" value="cricket"  onChange={onChangeValue} checked={gender === "cricket"}/>Cricket
+      <input type="radio" value="dance"  onChange={onChangeValue} checked={gender === "dance"}/>Dance
     </div>,
     <div>
       <h2><b>Q:2 Where Is Your City</b></h2>
-        <input type="radio"/>Navsari
-        <input type="radio"/>Surat
-        <input type="radio"/>Baroda
+        <input type="radio" value="navsari"  onChange={onChangeValue} checked={gender === "navsari"}/>Navsari
+        <input type="radio" value="surat"  onChange={onChangeValue} checked={gender === "surat"}/>Surat
+        <input type="radio" value="baroda"  onChange={onChangeValue} checked={gender === "baroda"}/>Baroda
     </div>,
 
     <div>
       <h2><b>Q:3 What is your Name</b></h2>
-      <input type="radio"/>Mayuri
-      <input type="radio"/>Resu
-      <input type="radio"/>Pratik
+      <input type="radio"value="Mayuri"  onChange={onChangeValue} checked={gender === "Mayuri"}/>Mayuri
+      <input type="radio" value="Resu"  onChange={onChangeValue} checked={gender === "Resu"}/>Resu
+      <input type="radio" value="Pratik"  onChange={onChangeValue} checked={gender === "Pratik"}/>Pratik
     </div>,
   ]
 
